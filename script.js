e7f73bbb573a02733d80acdab72b04d2c6fba6be
wajid ansari <112863100+ansariwajid9999@users.noreply.github.com>
2024-10-15 20:51:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-//your JS code here. If required.
+const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+
+checkboxes.forEach(checkbox => {
+    checkbox.addEventListener('change', () => {
+        let checkedCount = 0;
+
+        // Count checked checkboxes using a for loop
+        for (const cb of checkboxes) {
+            if (cb.checked) checkedCount++;
+        }
+
+        // Uncheck the current checkbox if the limit is exceeded
+        if (checkedCount > 2) {
+            checkbox.checked = false;
+            // alert('You can only select a maximum of 2 options.');
+        }
+    });
+});
